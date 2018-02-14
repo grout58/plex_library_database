2e6f568d5ffe8b6e6dde4427463c8e5fb05fa66c
Jason Grout <grout58@gmail.com>
2018-02-14 13:07:06 -0500

    added peewee model

---
+++ plex_model.py
@@ -0,0 +1,34 @@
+from peewee import *
+
+database = SqliteDatabase('plex_library.db', **{})
+
+class UnknownField(object):
+    def __init__(self, *_, **__): pass
+
+class BaseModel(Model):
+    class Meta:
+        database = database
+
+class Movies(BaseModel):
+    content_rating = CharField()
+    country = CharField()
+    directors = CharField()
+    duration = CharField()
+    genres = CharField()
+    last_viewed_at = CharField()
+    media = CharField(column_name='media_id')
+    rating = CharField()
+    release_date = CharField()
+    roles = CharField()
+    sort_title = CharField()
+    studio = CharField()
+    summary = CharField()
+    tagline = CharField()
+    title = CharField()
+    view_count = CharField()
+    writers = CharField()
+    year = CharField()
+
+    class Meta:
+        table_name = 'movies'
+
