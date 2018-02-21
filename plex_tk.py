4472329467aae6131f1b02c8b2a362e458d58742
= <grout58@gmail.com>
2018-02-20 22:22:37 -0500

    added tk label

--- plex_tk.py
+++ plex_tk.py
@@ -1,22 +1,29 @@
 from tkinter import *
 from plex_model import *
 
+movie_list = []
 
 def search():
     database.connect()
     for record in Movies.select().where(Movies.sort_title.contains(query.get())):
-        res_label.configure(text=record)
+        movie_list.append({record.sort_title: record.summary})
+    res_label.configure(text=movie_list[0])
+    return movie_list
+
     database.close()
 
 
 window = Tk()
+
+
+
 
 window.title('Plex Library Database')
 radMovie = Radiobutton(window, text='Movie', value=1)
 radTV = Radiobutton(window, text='TV', value=2)
 query = Entry(width=50)
 search_btn = Button(window, text='Search', command=search)
-res_label = (window)
+res_label = Label(window, text='blank')
 
 
 radMovie.grid(column=0, row=0, padx=5, pady=5)
