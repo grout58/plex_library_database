0bedbde5bddcf4740036933e927e404f77fb91f9
Grout <JGrout@tmn.int>
2018-02-21 15:29:51 -0500

    added forward and back buttons to window

--- plex_tk.py
+++ plex_tk.py
@@ -2,16 +2,46 @@ from tkinter import *
 from plex_model import *
 
 movie_list = []
+list_count = 0
+
+
 
 def search():
     database.connect()
     for record in Movies.select().where(Movies.sort_title.contains(query.get())):
         movie_list.append({record.sort_title: record.summary})
-    res_label.configure(text=movie_list[0])
-    return movie_list
-
+    for k, v in movie_list[list_count].items():
+        lblTitle.configure(text='Title:  {}'.format(k))
+        lblSummary.configure(text='Summary: {}'.format(v))
+        return list_count
+        
     database.close()
 
+def next_record():
+    try:
+        global list_count
+        list_count += 1
+        for k, v in movie_list[list_count].items():
+            lblTitle.configure(text='Title:  {}'.format(k))
+            lblSummary.configure(text='Summary: {}'.format(v))
+        return list_count
+    except IndexError:
+        messagebox.showinfo('No more movies', 'Out of Movies')
+
+def prev_record():
+    try:
+        global list_count
+        list_count -= 1
+        for k, v in movie_list[list_count].items():
+            lblTitle.configure(text='Title: {}'.format(k))
+            lblSummary.configure(text='Summary: {}'.format(v))
+        return list_count
+    except IndexError:
+        messagebox.showinfo('No more movies', 'Out of Movies')
+        
+                               
+            
+        
 
 window = Tk()
 
@@ -23,14 +53,20 @@ radMovie = Radiobutton(window, text='Movie', value=1)
 radTV = Radiobutton(window, text='TV', value=2)
 query = Entry(width=50)
 search_btn = Button(window, text='Search', command=search)
-res_label = Label(window, text='blank')
+next_btn = Button(window, text='Next', command=next_record)
+prev_btn = Button(window, text='Back', command=prev_record)
+lblTitle = Label(window, text='')
+lblSummary = Label(window, text='')
 
 
 radMovie.grid(column=0, row=0, padx=5, pady=5)
 radTV.grid(column=1, row=0, padx=5, pady=5)
-query.grid(column=0, row=1, padx=5, pady=20, columnspan=10)
+query.grid(column=0, row=1, padx=5, pady=20, columnspan=5)
 search_btn.grid(column=10, row=1, padx=5, pady=5)
-res_label.grid()
+next_btn.grid(column=11, row=1, padx=5, pady=5)
+prev_btn.grid(column=12, row=1, padx=5, pady=5)
+lblTitle.grid(column=0, row=3, padx=5, pady=5)
+lblSummary.grid(column=0, row=4, padx=5, pady=5)
 
 
 window.geometry('500x200')
